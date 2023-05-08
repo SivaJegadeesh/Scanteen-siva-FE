f783fedf1e5bcd51a288566281ca5b091994558a
SivaJegadeesh <128959325+SivaJegadeesh@users.noreply.github.com>
2023-05-08 11:30:58 +0530

    Delete lib directory

--- lib/core/app_images.dart
+++
@@ -1,5 +0,0 @@
-class AppImages {
-  static const String twitter = "assets/twitter.png";
-  static const String linkedin = "assets/linkedin.png";
-  static const String facebook = "assets/fb.png";
-}
--- lib/core/app_vectors.dart
+++
@@ -1,3 +0,0 @@
-class AppVectors {
-  static const String animatedLogo = "assets/animation_logo.svg";
-}
